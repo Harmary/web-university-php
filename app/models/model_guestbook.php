ba63ee9a3fa75c389a9a49bf0b3dd1529b331a7e
Harmary <maria.h4rlamova@yandex.ru>
2022-04-28 12:24:11 +0300

    Сделан парсинг файла в гостевой книге

--- app/models/model_guestbook.php
+++ app/models/model_guestbook.php
@@ -1,18 +1,38 @@
 <?
 
-class ModelGuestbook extends Model {
+class ModelGuestbook extends Model
+{
+
+    public $path = 'public/files/messages.inc.txt';
+
     function __construct()
     {
-        
+
     }
-    public function saveComment(){
-        if($_SERVER["REQUEST_METHOD"] === "POST"){
-            $message = PHP_EOL.$_POST['name'].';'.$_POST['email'].';'.$_POST['comment'];
-            $file = fopen('public/files/messages.inc.txt',"a+");
-            fwrite($file,$message);
+    public function saveComment()
+    {
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+            $message = $_POST['name'] . ';' . $_POST['email'] . ';' . $_POST['comment'].";".date("m.d.y").PHP_EOL;
+            $file = fopen($this->path, "a+");
+            fwrite($file, $message);
+            fclose($file);
         }
     }
-    function getFileText(){
-        
+
+    function getFileText()
+    {
+        $lines = file($this->path);
+        $records = array();
+        foreach ($lines as $line){
+            list($name,$email,$comment) = str_getcsv($line, ";");
+            $records[] = [
+                "name"=>$name,
+                "email"=>$email,
+                "comment"=>$comment,
+            ];
+        }
+
+        return $records;
     }
+
 }
--- app/views/guestbook_view.php
+++ app/views/guestbook_view.php
@@ -1,34 +1,43 @@
-<div class="content">
+
     <div class="t-contact">
-        <div class="col-6">
-            <h1 class="main-title">Оставь свой отзыв </h1>
-        </div>
-    </div>
-    <section class="col-6 justify-content-left">
-        <form class="form needs-validation" name="form" id="form" method="post">
-            <div class="form-group contact__name">
-                <h2>ФИО</h2>
-                <input class="form-control" name="name" id="fio" type="text" value="<?php echo $data->fields['name'] ?>" placeholder="Иванов Иван Иванович" >
-                <?php echo $data->validator->messages['name'] ?>
-            </div>
-            <div class="form-group contact__email">
-                <h2 for="form-email">Электронная почта</h2>
-                <input type="email" class="form-control" name="email" id="form-email" value="<?php echo $data->fields['email'] ?>" placeholder="name@example.com">
-                <?php echo $data->validator->messages['email'] ?>
-            </div>
-            <div class="form-group contact__telephone">
-                <h2 for="form-telephone">Отзыв</h2>
-                <input type="text" class="form-control" name="comment" value="<?php echo $data->fields['comment'] ?>" id="form-comment" placeholder="оставьте ваш отзыв">
-                <?php echo $data->validator->messages['comment'] ?>
-            </div>
-            <div class="error"></div>
-            <input class="btn btn-primary" type="submit" value="Submit" id="submit">
-            <input class="btn btn-primary" type="reset" value="Reset">
-        </form>
-    </section>
-    <section class="comments">
         
-    </section>
-</div>
-<!-- <script src="js/contacts.js"></script> -->
-<script src="js/calendar.js"></script>
+            <h1 class="main-title">Оставь свой отзыв </h1>
+
+    </div>
+
+    <div class="container">
+        <section class="comment-form">
+            <form class="form needs-validation" name="form" id="form" method="post">
+                <div class="form-group contact__name">
+                    <h2>ФИО</h2>
+                    <input class="form-control" name="name" id="fio" type="text" value="<?php echo $data->fields['name'] ?>" placeholder="Иванов Иван Иванович">
+                    <?php echo $data->validator->messages['name'] ?>
+                </div>
+                <div class="form-group contact__email">
+                    <h2 for="form-email">Электронная почта</h2>
+                    <input type="email" class="form-control" name="email" id="form-email" value="<?php echo $data->fields['email'] ?>" placeholder="name@example.com">
+                    <?php echo $data->validator->messages['email'] ?>
+                </div>
+                <div class="form-group contact__telephone">
+                    <h2 for="form-telephone">Отзыв</h2>
+                    <input type="text" class="form-control" name="comment" value="<?php echo $data->fields['comment'] ?>" id="form-comment" placeholder="оставьте ваш отзыв">
+                    <?php echo $data->validator->messages['comment'] ?>
+                </div>
+                <div class="error"></div>
+                <input class="btn btn-primary" type="submit" value="Submit" id="submit">
+                <input class="btn btn-primary" type="reset" value="Reset">
+            </form>
+        </section>
+        <section class="comment">
+            <? foreach ($records = $data->getFileText() as $record) : ?>
+                <div class="comment__item">
+                    <p>Имя: <?= $record['name'] ?></p>
+                    <p>Email: <?= $record['email'] ?></p>
+                    <p>Сообщение: <?= $record['comment'] ?></p>
+                </div>
+            <? endforeach ?>
+        </section>
+    </div>
+    </div>
+    <!-- <script src="js/contacts.js"></script> -->
+    <script src="js/calendar.js"></script>
